14e3a8f5133c91d0db6fd85a5d1db2129366b2c4
rgsk <guptarahul70322@gmail.com>
2023-07-16 22:53:47 +0530

    firebase packages and setup

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,10 +5,18 @@
 //  Created by Rahul Gupta on 16/07/23.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+
 
 @main
 struct ToDoListApp: App {
+    // register app delegate for Firebase setup
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()

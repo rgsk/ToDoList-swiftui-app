718b74cb1be6657dcd8a75ff1ce335f36c59f24a
rgsk <guptarahul70322@gmail.com>
2023-07-16 22:39:07 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Rahul Gupta on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
